d49c34d40ee34f893ffa8b20db0f557ba16a2dfc
SweelLok <ukr.vadya@gmail.com>
2025-03-03 20:42:52 +0200

    4 commit

--- app/routes/games.py
+++ app/routes/games.py
@@ -16,17 +16,19 @@ def get_games_json():
         return jsonify(games)
     
     return jsonify({"error": "Error to get db data"}), 500
- 
+
 @app.get("/menu/")
 def get_games():
+    page = request.args.get("page", 1, type=int)
     query = request.args.get("search", "")
-    url = f"{RAWG_BASE_URL}/games?key={RAWG_API_KEY}&search={query}"
+    url = f"{RAWG_BASE_URL}/games?key={RAWG_API_KEY}&search={query}&page={page}&page_size=21"
     
     response = requests.get(url)
     
     if response.status_code == 200:
         games = response.json().get("results", [])
-        return render_template("menu.html", games=games)
-    else:
-        print(response.json())
-        return render_template("menu.html", games=[])
+        total_results = response.json().get("count", 0)
+        total_pages = (total_results // 21) + (1 if total_results % 21 > 0 else 0)
+        return render_template("menu.html", games=games, page=page, total_pages=total_pages, max=max, min=min, query=query)
+    
+    return render_template("menu.html", games=[], page=page, total_pages=0, max=max, min=min, query=query)
